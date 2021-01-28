fdad68b8ca315653b5ea03783d27cf19ef87a9b4
y4shiro <y4shironao@gmail.com>
2021-01-28 11:00:38 +0900

    コマンドライン引数で --gfm オプションを受け付けるよう実装

--- use-case/nodecli/main.js
+++ use-case/nodecli/main.js
@@ -2,8 +2,14 @@ const program = require("commander");
 const fs = require("fs");
 const marked = require("marked");
 
+program.option("--gfm", "GFMを有効にする");
 program.parse(process.argv);
 const filePath = program.args[0];
+
+const cliOptions = {
+  gfm: false,
+  ...program.opts(),
+};
 
 fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
   if (err) {
@@ -11,6 +17,8 @@ fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
     process.exit(1);
     return;
   }
-  const html = marked(file);
+  const html = marked(file, {
+    gfm: cliOptions.gfm,
+  });
   console.log(html);
 });
