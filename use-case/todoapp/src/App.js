a052a1d3705771287252cb6ea901abf01c060bf5
y4shiro <y4shironao@gmail.com>
2021-01-29 19:25:20 +0900

    チェックボックスで Todo の完了状態を管理

--- use-case/todoapp/src/App.js
+++ use-case/todoapp/src/App.js
@@ -19,14 +19,26 @@ export class App {
       const todoItems = this.todoListModel.getTodoItems();
       // それぞれの TodoItem 要素を TodoListElement 以下へ追加する
       todoItems.forEach(item => {
-        const todoItemElement = element`<li>${item.title}</li>`;
+        // 完了済みならchecked属性をつけ、未完了ならchecked属性を外す
+        const todoItemElement = item.completed
+          ? element`<li><input type="checkbox" class="checkbox" checked><s>${item.title}</s></li>`
+          : element`<li><input type="checkbox" class="checkbox">${item.title}</li>`;
+        // チェックボックスがトグルしたときのイベントにリスナー関数を登録
+        const inputCheckboxElement = todoItemElement.querySelector(".checkbox");
+        inputCheckboxElement.addEventListener("change", () => {
+          // 指定したTodoアイテムの完了状態を反転させる
+          this.todoListModel.updateTodo({
+            id: item.id,
+            completed: !item.completed
+          });
+        });
         todoListElement.appendChild(todoItemElement);
       });
       // containerElement の中身を todoListElement で上書きする
       render(todoListElement, containerElement);
       // アイテム数の表示を更新
       todoItemCountElement.textContent = `Todoアイテム数: ${this.todoListModel.getTotalCount()}`;
-    })
+    });
     // フォームを送信したら、新しい TodoItemModel を追加する
     formElement.addEventListener("submit", (event) => {
       event.preventDefault();
